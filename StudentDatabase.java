4dca63a21e39331c324197767a1513df2f20f23e
shalinikannadhasan-1999 <90966650+shalinikannadhasan-1999@users.noreply.github.com>
2021-10-10 21:43:30 +0530

    Add files via upload

---
+++ StudentDatabase.java
@@ -0,0 +1,15 @@
+package com.jdbc.student;
+
+public class StudentDatabase {
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			
+			Class.forName("com.mysql.cj.Driver");
+		}
+		catch(Exception e)
+	}
+
+}
